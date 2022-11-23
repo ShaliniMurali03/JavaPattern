09cce1e3c99d849d326c9bc2c6cf61007f4fd179
ShaliniMurali03 <117591791+ShaliniMurali03@users.noreply.github.com>
2022-11-24 00:22:01 +0530

    Create Number_Pattern.java
    version 1.0

---
+++ Number_Pattern.java
@@ -0,0 +1,16 @@
+public class Number_Pattern
+{
+    public static void main(String[] args)
+    {
+        int n = 5;
+        int i , j;
+        for(i=1;i<n;i++)
+        {
+            for(j=1;j<n;j++)
+            {
+                    System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
