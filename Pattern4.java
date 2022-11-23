4c2d5bff4cba121e9d9ae1d6d25552505ca212c3
ShaliniMurali03 <117591791+ShaliniMurali03@users.noreply.github.com>
2022-11-24 00:23:01 +0530

    Create Pattern4.java
    Version 1.0

---
+++ Pattern4.java
@@ -0,0 +1,22 @@
+public class Pattern4
+{
+    public static void main(String[] args)
+    {
+        int n = 13;
+        int i, j;
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if((j==0 && i>=(n-1)/2)||(j==n-1 && i>=(n-1)/2)||i==n-1 || (i+j)>=(n-1)+(n-1)/2 || i-j>= (n-1)/2)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }System.out.println();
+        }
+    }
+}
