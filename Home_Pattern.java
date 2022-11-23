9b9649290be4426d7f4d232997dea43b8f76938e
ShaliniMurali03 <117591791+ShaliniMurali03@users.noreply.github.com>
2022-11-24 00:22:32 +0530

    Create Home_Pattern.java
    version1.0

---
+++ Home_Pattern.java
@@ -0,0 +1,22 @@
+public class Home_Pattern
+{
+    public static void main(String[] args)
+    {
+        int n = 13;
+        int i, j;
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if(i==0||j==0||j==n-1||i==n-1||(i+j<= (n-1)/2)||j-i>=(n-1)/2)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            } System.out.println();
+        }
+    }
+}
